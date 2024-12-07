645b5bbd4f1f68850be67b98046d2a5681e7ad71
Marko <49600278+clearfeld@users.noreply.github.com>
2024-12-06 22:20:48 -0500

    Add: chorus plugin list

---
+++ src/pages/plugins/lists/chorus.tsx
@@ -0,0 +1,55 @@
+import type { T_Plugin } from ".";
+
+export const chorus_list_meta = {
+	title: "Chorus",
+};
+
+export const chorus_list: T_Plugin[] = [
+	{
+		title: "Magic Switch",
+		extensions: ["VST3", "VST2", "AU", "AAX"],
+		paid: "FREE",
+		brand: "Baby Audio",
+		img: "https://images.squarespace-cdn.com/content/v1/561e2985e4b08862a3496b82/c79b082d-1060-4bd4-8719-71450737ec25/Baby%2BAudio%2BMagic%2BDice%2BFront%2BPage+2.png?format=2500w",
+		homepage: "https://babyaud.io/freebies",
+		download: "https://babyaud.io/freebies",
+		notes: "",
+	},
+
+	{
+		title: "_Chorus JUN-6",
+		extensions: ["VST3", "VST2", "AU", "AAX"],
+		paid: "PAID",
+		brand: "Arturia",
+		img: "https://medias.arturia.net/cdn-cgi/image/quality=80/images/products/chorus-jun-6/banner-image.png",
+		homepage:
+			"https://www.arturia.com/products/software-effects/chorus-jun-6/overview",
+		download: "https://www.arturia.com/store/software-effects/chorusjun-6",
+		notes: "",
+	},
+
+	{
+		title: "_Chorus Dimension-D",
+		extensions: ["VST3", "VST2", "AU", "AAX"],
+		paid: "PAID",
+		brand: "Arturia",
+		img: "https://medias.arturia.net/cdn-cgi/image/quality=80/images/products/chorus-dimension-d/banner-image.png",
+		homepage:
+			"https://www.arturia.com/products/software-effects/chorus-dimension-d/overview",
+		download:
+			"https://www.arturia.com/store/software-effects/chorusdimension-d",
+		notes: "",
+	},
+
+	{
+		title: "Valhalla Space Modulator",
+		extensions: ["VST3", "VST2", "AAX"],
+		paid: "FREE",
+		brand: "Valhalla DSP",
+		img: "https://val-media-offload.s3.amazonaws.com/wp-content/uploads/2016/06/08073920/ValhallaSpaceModGUI-960x437.jpg.webp",
+		homepage:
+			"https://valhalladsp.com/shop/modulation/valhalla-space-modulator/",
+		download: "https://valhalladsp.com/demos-downloads/",
+		notes: "",
+	},
+];
