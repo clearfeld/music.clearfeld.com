24e9df9314b3dbf0c1091b89a46cec9f5dd393f4
Marko <49600278+clearfeld@users.noreply.github.com>
2024-12-06 22:20:38 -0500

    Add: bitcrusher plugin list

---
+++ src/pages/plugins/lists/bitcrusher.tsx
@@ -0,0 +1,18 @@
+import type { T_Plugin } from ".";
+
+export const bitcrusher_list_meta = {
+	title: "Bitcrusher",
+};
+
+export const bitcrusher_list: T_Plugin[] = [
+	{
+		title: "Decimort 2",
+		extensions: ["VST3", "VST2", "AU", "AAX"],
+		paid: "PAID",
+		brand: "D16 Group",
+		img: "https://d16.pl/images/products/decimort2/overview/gui.jpg",
+		homepage: "https://d16.pl/decimort2",
+		download: "https://d16.pl/decimort2",
+		notes: "",
+	},
+];
